49510b34f83ef4b4ded18201673ef32edaa0df6d
Cristian Szabo <cristi_bomfly@outlook.com>
2021-07-24 18:50:51 +0300

    flask_server: Login API with JWT

---
+++ src/webserver/routes/authentication.py
@@ -0,0 +1,76 @@
+"""
+API Route class.
+"""
+
+__version__ = "0.0.1"
+__author__ = "Szabo Cristian"
+
+import datetime
+
+import jwt
+from flask import request
+from flask_restplus import Resource
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from webserver.core.users_management import UsersManagementAPI
+from webserver.flask_rest import FlaskRestPlusApi, FlaskApp
+from webserver.models.user import User
+from webserver.responses import response_400, response
+from webserver.routes.utils import api_param_form, get_request_parameter
+
+api = FlaskRestPlusApi.get_instance()
+app = FlaskApp.get_instance()
+
+
+class RouteLogin(Resource):
+
+    @api.doc(responses={
+        201: "OK",
+        401: "Could not verify authorization."
+    })
+    def post(self) -> response:
+        auth = request.authorization
+        if not auth or not auth.username or not auth.password:
+            return response(401, {'WWW-Authenticate': 'Basic realm="Login required!"'},
+                            'Could not verify authorization.')
+
+        user = User.query.filter_by(username=auth.username).first()
+        if not user:
+            return response(401, {'WWW-Authenticate': 'Basic realm="Login required!"'},
+                            'Could not verify authorization.')
+
+        if not check_password_hash(user.password, auth.password):
+            return response(401, {'WWW-Authenticate': 'Basic realm="Login required!"'},
+                            'Could not verify authorization.')
+
+        token = jwt.encode(
+            {'public_id': user.public_id,
+             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+            app.config["SECRET_KEY"])
+        
+        return response(201, {'token': token}, 'OK')
+
+
+class RouteRegister(Resource):
+
+    @api.doc(params={
+        "username": api_param_form(required=True, description="Username"),
+        "password": api_param_form(required=True, description="Password"),
+    })
+    @api.doc(responses={
+        201: "OK",
+        400: "Param <> is required | Password must have more than 3 characters",
+        409: "Username already exists.",
+        500: "Could not create user."
+    })
+    def post(self) -> response:
+        username, msg = get_request_parameter(name="username", expected_type=str, required=True)
+        if not username:
+            return response_400(msg)
+
+        password, msg = get_request_parameter(name="password", expected_type=str, required=True)
+        if not password:
+            return response_400(msg)
+        hashed_password = generate_password_hash(password=password, method='sha256')
+
+        return response(*UsersManagementAPI.create_user(username=username, hashed_password=hashed_password))
--- src/webserver/server.py
+++ src/webserver/server.py
@@ -1,4 +1,5 @@
 from webserver.flask_rest import FlaskApp, FlaskRestPlusApi
+from webserver.routes.authentication import RouteLogin, RouteRegister
 from webserver.routes.investment_calculator import RouteInvestmentCalculatorReturnRate, \
     RouteInvestmentCalculatorCompoundInterest
 from webserver.routes.stock_prices import RouteStockPrices
@@ -10,6 +11,9 @@ flask_app = FlaskApp.get_instance()
 flask_api = FlaskRestPlusApi.get_instance()
 
 flask_api.ns('users').add_resource(RouteUsers, "/users")
+
+flask_api.ns('auth').add_resource(RouteLogin, "/login")
+flask_api.ns('auth').add_resource(RouteRegister, "/register")
 
 flask_api.ns('stocks').add_resource(RouteStocks, "/stocks")
 flask_api.ns('stock_prices').add_resource(RouteStockPrices, "/stock-prices")
