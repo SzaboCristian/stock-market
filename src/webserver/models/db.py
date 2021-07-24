994074a77a4fe68550ca0860ac7f1105816545da
Cristian Szabo <cristi_bomfly@outlook.com>
2021-07-24 20:18:13 +0300

    flask_server: Singleton SQLAlchemy

--- src/webserver/models/db.py
+++ src/webserver/models/db.py
@@ -1,4 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# TODO - make singleton db = DB.get_instance() and return that way so we won't have many connections
-db = SQLAlchemy()
+
+class SQLAlchemyDB:
+    db_instance = None
+
+    def __init__(self):
+        raise Exception('FlaskApp constructor called directly. Use get_instance() method.')
+
+    @staticmethod
+    def get_instance() -> SQLAlchemy:
+        if SQLAlchemyDB.db_instance is None:
+            SQLAlchemyDB.db_instance = SQLAlchemy()
+
+        return SQLAlchemyDB.db_instance
+
+
+db = SQLAlchemyDB.get_instance()
