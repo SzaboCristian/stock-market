ecced7614a9ba3ac068d70c9c942807b33f6f15c
SzaboCristian <cristi_bomfly@outlook.com>
2021-10-16 13:04:03 +0300

    flask_server: wsgi - register db with gunicorn bugfix

--- src/webserver/wsgi.py
+++ src/webserver/wsgi.py
@@ -11,21 +11,21 @@ from webserver.daemons.stock_prices_daemon import stock_prices_updater_task
 from webserver.models.db import db
 from webserver.server import flask_app
 
+# setup logger
+webserver_logger = logging.getLogger("webserver.error")
+flask_app.logger.handlers = webserver_logger.handlers
+flask_app.logger.setLevel(webserver_logger.level)
+
+# init users db
+db.app = flask_app
+db.init_app(flask_app)
+db.create_all()
+
+# create crcrc inserter polling thread
+stock_prices_updater_thread = Thread(target=stock_prices_updater_task)
+stock_prices_updater_thread.setDaemon(True)
+stock_prices_updater_thread.start()
+
 if __name__ == '__main__':
-    # setup logger
-    webserver_logger = logging.getLogger("webserver.error")
-    flask_app.logger.handlers = webserver_logger.handlers
-    flask_app.logger.setLevel(webserver_logger.level)
-
-    # init users db
-    db.app = flask_app
-    db.init_app(flask_app)
-    db.create_all()
-
-    # create crcrc inserter polling thread
-    stock_prices_updater_thread = Thread(target=stock_prices_updater_task)
-    stock_prices_updater_thread.setDaemon(True)
-    stock_prices_updater_thread.start()
-
     # run app - only when run without gunicorn
     flask_app.run(threaded=True, debug=(os.getenv("DEPLOYED") == "False"), host="0.0.0.0", port=5000)
